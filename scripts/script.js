b2f8dde33a9721578f0466720fc28a1e1cfabbbd
Johannes120 <johnnymotsemme6@gmail.com>
2026-03-23 14:26:34 +0200

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -4,13 +4,13 @@ import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } f
 
 // Firebase config
 const firebaseConfig = {
-  apiKey: "AIzaSyBkiWhymnICIdCQsZQC-y4E45XRiYQOkio",
-  authDomain: "people-s-bank-web.firebaseapp.com",
-  projectId: "people-s-bank-web",
-  storageBucket: "people-s-bank-web.firebasestorage.app",
-  messagingSenderId: "789021638199",
-  appId: "1:789021638199:web:66201c5b05b9aa5c0f49b2",
-  measurementId: "G-BJXYPBBH1X"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
